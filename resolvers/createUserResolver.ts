3540ba0267c36c666b086112d7104e28001aa7d7
L-Nicolas <nicolas.lacoste.esgi@gmail.com>
2023-10-13 00:01:50 +0200

    Heroku

--- resolvers/createUserResolver.ts
+++ resolvers/createUserResolver.ts
@@ -1,20 +1,29 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import database from '../database';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import database from '../database'
 
 const createUserResolver = {
   Mutation: {
-    createUser: async ({ username, password }: { username: string, password: string }) => {
+    createUser: async ({
+      username,
+      password,
+    }: {
+      username: string
+      password: string
+    }) => {
       // Vérifier d'abord si le nom d'utilisateur existe déjà
-      const { data: existingUser } = await database.from('Users').select('*').eq('username', username);
+      const { data: existingUser } = await database
+        .from('Users')
+        .select('*')
+        .eq('username', username)
 
       if (existingUser && existingUser.length > 0) {
-        throw new Error('Un utilisateur avec ce nom d\'utilisateur existe déjà.');
+        throw new Error("Un utilisateur avec ce nom d'utilisateur existe déjà.")
       }
 
       // Si le nom d'utilisateur n'existe pas, alors on peut créer l'utilisateur
       // Hasher le mot de passe (vous pouvez utiliser Bcrypt pour cela)
-      const hashedPassword = await bcrypt.hash(password, 10);
+      const hashedPassword = await bcrypt.hash(password, 10)
 
       // Insérer le nouvel utilisateur dans la base de données
       const { data, error } = await database.from('Users').upsert([
@@ -22,15 +31,15 @@ const createUserResolver = {
           username,
           password: hashedPassword,
         },
-      ]);
+      ])
 
       if (error) {
-        throw new Error('Échec de la création de l\'utilisateur' + error.message);
+        throw new Error("Échec de la création de l'utilisateur" + error.message)
       }
 
-      return data[0];
+      return data[0]
     },
   },
-};
+}
 
-export default createUserResolver;
+export default createUserResolver
--- resolvers/loginResolver.ts
+++ resolvers/loginResolver.ts
@@ -1,31 +1,39 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import database from '../database';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import database from '../database'
 
 const loginResolver = {
   Mutation: {
-    login: async ({ username, password }: { username: string, password: string }) => {
+    login: async ({
+      username,
+      password,
+    }: {
+      username: string
+      password: string
+    }) => {
       // Rechercher l'utilisateur par son nom d'utilisateur
-      const { data: users } = await database.from('Users').select('*').eq('username', username);
+      const { data: users } = await database
+        .from('Users')
+        .select('*')
+        .eq('username', username)
 
       if (users && users.length === 0) {
-        throw new Error('Nom d\'utilisateur incorrect.');
+        throw new Error("Nom d'utilisateur incorrect.")
       }
 
-      
-      const user = users![0];
+      const user = users![0]
 
-      const passwordMatch = await bcrypt.compare(password, user.password);
+      const passwordMatch = await bcrypt.compare(password, user.password)
 
       if (!passwordMatch) {
-        throw new Error('Mot de passe incorrect.');
+        throw new Error('Mot de passe incorrect.')
       }
 
-      const token = jwt.sign({ userId: user.id }, 'votre_secret_jwt'); // Remplacez 'votre_secret_jwt' par une clé secrète réelle
+      const token = jwt.sign({ userId: user.id }, 'votre_secret_jwt') // Remplacez 'votre_secret_jwt' par une clé secrète réelle
 
-      return { token, user };
+      return { token, user }
     },
   },
-};
+}
 
-export default loginResolver;
+export default loginResolver
